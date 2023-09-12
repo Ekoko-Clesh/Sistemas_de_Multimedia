9ee152f18cc059f66061b3d9a090c325aa8b08c3
Ekoko Clesh <ekokoclesh07@gmail.com>
2023-09-12 21:08:28 +0200

    Exibir os Zeros da sequencia

---
+++ Supressao_de_zeros_2.py
@@ -0,0 +1,19 @@
+def codificador(ci):
+    codigo = ""
+    cod_input = str(ci)
+    i = 0
+    while i < len(cod_input):
+        cont = 1
+        while i < len(cod_input)-1 and cod_input[i] == cod_input[i+1]:
+            cont +=1
+            print(cod_input[i], end="")
+            i += 1
+        if cont > 1:
+            codigo += "!" +str(cont)
+        else:
+            codigo += cod_input[i]
+        i += 1
+    return codigo
+
+retorno = codificador(10400000000234)
+# print(retorno)
